86c703a164aa6adcbdfbacef145165d004e95fe1
Krev3tka <179160528+Krev3tka@users.noreply.github.com>
2026-05-27 13:56:46 +0300

    feat: add support for cli flags (-pony and -size)

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"flag"
 	"ponyfetch/internal/fetch"
 )
 
 func main() {
-	fetch.PrintFetch("applejack", "normal")
+	var ponyName string
+	var ponySize string
+
+	flag.StringVar(&ponyName, "p", "twilight", "Name of the pony for ASCII art")
+	flag.StringVar(&ponyName, "pony", "twilight", "Name of the pony for ASCII art")
+
+	flag.StringVar(&ponySize, "s", "normal", "Size of the pony ASCII art (normal, little)")
+	flag.StringVar(&ponySize, "size", "normal", "Size of the pony ASCII art (normal, little)")
+
+	flag.Parse()
+
+	fetch.PrintFetch(ponyName, ponySize)
 }
