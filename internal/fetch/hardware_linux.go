2ec361c6d5f4093d6b77439d433fc1bee59e9c10
Krev3tka <179160528+Krev3tka@users.noreply.github.com>
2026-05-27 14:30:06 +0300

    feat: implement native /proc parsing for Linux systems

--- internal/fetch/hardware_linux.go
+++ internal/fetch/hardware_linux.go
@@ -1,3 +1,63 @@
 //go:build linux
 
 package fetch
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func GetCPU() string {
+	file, err := os.Open("/proc/сpuinfo")
+	if err != nil {
+		return "Unknown CPU"
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	line := scanner.Text()
+	if strings.HasPrefix(line, "model name") {
+		parts := strings.Split(line, ":")
+		if len(parts) > 1 {
+			return strings.TrimSpace(parts[1])
+		}
+	}
+
+	return "Unknown CPU"
+}
+
+func parseMeminfo() (totalGB, usedGB float64) {
+	file, err := os.Open("/proc/meminfo")
+	if err != nil {
+		return 0, 0
+	}
+	defer file.Close()
+
+	var totalMem, availableMem int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "MemTotal:") {
+			fmt.Sscanf(line, "MemTotal: %d kB", &totalMem)
+		}
+		if strings.HasPrefix(line, "MemAvailable:") {
+			fmt.Sscanf(line, "MemAvailable: %d kB", &availableMem)
+		}
+	}
+
+	totalGB = float64(totalMem) / 1024 / 1024
+	usedGB = float64(totalMem-availableMem) / 1024 / 1024
+	return totalGB, usedGB
+}
+
+func GetTotalRAM() string {
+	total, _ := parseMeminfo()
+	return fmt.Sprintf("%.2f GB", total)
+}
+
+func GetUsedRAM() string {
+	_, used := parseMeminfo()
+	return fmt.Sprintf("%.2f GB", used)
+}
